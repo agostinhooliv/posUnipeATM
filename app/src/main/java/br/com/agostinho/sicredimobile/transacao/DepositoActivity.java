3a6f605c869992cd451e52b58ce419adb690e9a5
Breno Jeronimo <brenojsm@gmail.com>
2017-02-21 20:50:48 -0300

    cria o layoutda activity de deposito

--- app/src/main/java/br/com/agostinho/sicredimobile/transacao/DepositoActivity.java
+++ app/src/main/java/br/com/agostinho/sicredimobile/transacao/DepositoActivity.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import br.com.agostinho.sicredimobile.R;
+import br.com.agostinho.sicredimobile.util.BaseActivity;
 
-public class DepositoActivity extends AppCompatActivity {
+public class DepositoActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deposito);
+
+        setUpToolbar();
+        setTitleToolbar("Deposito");
+        setDisplayHomeAsUpEnabled(true);
 
     }
 }
