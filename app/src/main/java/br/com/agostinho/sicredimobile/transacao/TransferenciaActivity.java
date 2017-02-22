7b4a3b6603d23b825c1145a8e0da7b6943e0f5da
Breno Jeronimo <brenojsm@gmail.com>
2017-02-21 22:38:31 -0300

    configura o toolbar da activity de transferencia

--- app/src/main/java/br/com/agostinho/sicredimobile/transacao/TransferenciaActivity.java
+++ app/src/main/java/br/com/agostinho/sicredimobile/transacao/TransferenciaActivity.java
@@ -4,12 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import br.com.agostinho.sicredimobile.R;
+import br.com.agostinho.sicredimobile.util.BaseActivity;
 
-public class TransferenciaActivity extends AppCompatActivity {
+public class TransferenciaActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transferencia);
+
+        setUpToolbar();
+        setTitleToolbar("Transferencia");
+        setDisplayHomeAsUpEnabled(true);
     }
 }
